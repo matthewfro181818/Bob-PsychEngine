d38f3f8bf3bdf75385891bf8a53ee325c14b106f
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2026-01-16 12:22:38 -0500

    Update HealthIcon.hx

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -6,11 +6,43 @@ class HealthIcon extends FlxSprite
 	private var isPlayer:Bool = false;
 	private var char:String = '';
 
-	public function new(char:String = 'face', isPlayer:Bool = false, ?allowGPU:Bool = true)
+	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
 		super();
-		this.isPlayer = isPlayer;
-		changeIcon(char, allowGPU);
+		
+		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+
+		antialiasing = true;
+		animation.add('bf', [0, 1], 0, false, isPlayer);
+		animation.add('bf-car', [0, 1], 0, false, isPlayer);
+		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
+		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
+		animation.add('spooky', [2, 3], 0, false, isPlayer);
+		animation.add('pico', [4, 5], 0, false, isPlayer);
+		animation.add('mom', [6, 7], 0, false, isPlayer);
+		animation.add('mom-car', [6, 7], 0, false, isPlayer);
+		animation.add('tankman', [8, 9], 0, false, isPlayer);
+		animation.add('face', [10, 11], 0, false, isPlayer);
+		animation.add('dad', [12, 13], 0, false, isPlayer);
+		animation.add('senpai', [22, 22], 0, false, isPlayer);
+		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
+		animation.add('spirit', [23, 23], 0, false, isPlayer);
+		animation.add('bf-old', [14, 15], 0, false, isPlayer);
+		animation.add('gf', [16], 0, false, isPlayer);
+		animation.add('gf-christmas', [16], 0, false, isPlayer);
+		animation.add('gf-pixel', [16], 0, false, isPlayer);
+		animation.add('parents-christmas', [17, 18], 0, false, isPlayer);
+		animation.add('monster', [19, 20], 0, false, isPlayer);
+		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+		animation.add('bob', [24, 25], 0, false, isPlayer);
+		animation.add('angrybob', [26, 27], 0, false, isPlayer);
+		animation.add('hellbob', [28, 29], 0, false, isPlayer);
+		animation.add('ron', [30, 31], 0, false, isPlayer);
+		animation.add('glitched-bob', [34, 35], 0, false, isPlayer);
+		animation.add('gloop-bob', [32, 33], 0, false, isPlayer);
+		animation.add('little-man', [36, 37], 0, false, isPlayer);
+		animation.add('pizza', [38, 39], 0, false, isPlayer);
+		animation.play(char);
 		scrollFactor.set();
 	}
 
@@ -19,7 +51,7 @@ class HealthIcon extends FlxSprite
 		super.update(elapsed);
 
 		if (sprTracker != null)
-			setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
 	}
 
 	private var iconOffsets:Array<Float> = [0, 0];
