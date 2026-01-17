ada5d46a5c33db4c4b1f9f4aee6eea9d3c00f9c7
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2026-01-17 13:15:13 -0500

    Create TroubleStage.hx

---
+++ source/states/stages/TroubleStage.hx
@@ -0,0 +1,191 @@
+package states.stages;
+
+import states.stages.objects.*;
+
+class TroubleStage extends BaseStage
+{
+	// If you're moving your stage from PlayState to a stage file,
+	// you might have to rename some variables if they're missing, for example: camZooming -> game.camZooming
+
+	override function create()
+	{
+					var bg:FlxSprite = new FlxSprite(-100,10).loadGraphic(Paths.image('bob/nothappy_sky'));
+					bg.updateHitbox();
+					bg.scale.x = 1.2;
+					bg.scale.y = 1.2;
+					bg.active = false;
+					bg.antialiasing = true;
+					bg.scrollFactor.set(0.1, 0.1);
+					add(bg);
+					
+					var ground:FlxSprite = new FlxSprite(-537, -250).loadGraphic(Paths.image('bob/nothappy_ground'));
+					ground.updateHitbox();
+					ground.active = false;
+					ground.antialiasing = true;
+					add(ground);
+
+					var deadron:FlxSprite = new FlxSprite(-700, 600).loadGraphic(Paths.image('bob/GoodHeDied'));
+					deadron.updateHitbox();
+					deadron.active = false;
+					deadron.scale.x = 0.8;
+					deadron.scale.y = 0.8;
+					deadron.antialiasing = true;
+					add(deadron);
+	}
+	
+	override function createPost()
+	{
+		// Use this function to layer things above characters!
+	}
+
+	override function update(elapsed:Float)
+	{
+		// Code here
+	}
+
+	override function destroy()
+	{
+		// Code here
+	}
+
+	
+	override function countdownTick(count:Countdown, num:Int)
+	{
+		switch(count)
+		{
+			case THREE: //num 0
+			case TWO: //num 1
+			case ONE: //num 2
+			case GO: //num 3
+			case START: //num 4
+		}
+	}
+
+	override function startSong()
+	{
+		// Code here
+	}
+
+	// Steps, Beats and Sections:
+	//    curStep, curDecStep
+	//    curBeat, curDecBeat
+	//    curSection
+	override function stepHit()
+	{
+		// Code here
+	}
+	override function beatHit()
+	{
+		if (curSong.toLowerCase() == 'trouble' && curBeat == 504 )
+		{
+			BobIngameTransform();
+		}
+  }
+	override function sectionHit()
+	{
+		// Code here
+	}
+
+	function BobIngameTransform()
+	{
+		//screw you aether i want to fix the ingame cutscene
+		dad.playAnim('Transform', true);
+		FlxG.sound.play(Paths.sound('bobSpooky'));
+		var black:FlxSprite = new FlxSprite(-100, -100).makeGraphic(FlxG.width * 2, FlxG.height * 2, FlxColor.BLACK);
+		black.scrollFactor.set();
+		new FlxTimer().start(1.7, function(tmr:FlxTimer)
+		{
+			add(black);
+			FlxG.camera.fade(FlxColor.WHITE, 0.1, true);
+		});
+
+	}
+
+	// Substates for pausing/resuming tweens and timers
+	override function closeSubState()
+	{
+		if(paused)
+		{
+			//timer.active = true;
+			//tween.active = true;
+		}
+	}
+
+	override function openSubState(SubState:flixel.FlxSubState)
+	{
+		if(paused)
+		{
+			//timer.active = false;
+			//tween.active = false;
+		}
+	}
+
+	// For events
+	override function eventCalled(eventName:String, value1:String, value2:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float)
+	{
+		switch(eventName)
+		{
+			case "My Event":
+		}
+	}
+	override function eventPushed(event:objects.Note.EventNote)
+	{
+		// used for preloading assets used on events that doesn't need different assets based on its values
+		switch(event.event)
+		{
+			case "My Event":
+				//precacheImage('myImage') //preloads images/myImage.png
+				//precacheSound('mySound') //preloads sounds/mySound.ogg
+				//precacheMusic('myMusic') //preloads music/myMusic.ogg
+		}
+	}
+	override function eventPushedUnique(event:objects.Note.EventNote)
+	{
+		// used for preloading assets used on events where its values affect what assets should be preloaded
+		switch(event.event)
+		{
+			case "My Event":
+				switch(event.value1)
+				{
+					// If value 1 is "blah blah", it will preload these assets:
+					case 'blah blah':
+						//precacheImage('myImageOne') //preloads images/myImageOne.png
+						//precacheSound('mySoundOne') //preloads sounds/mySoundOne.ogg
+						//precacheMusic('myMusicOne') //preloads music/myMusicOne.ogg
+
+					// If value 1 is "coolswag", it will preload these assets:
+					case 'coolswag':
+						//precacheImage('myImageTwo') //preloads images/myImageTwo.png
+						//precacheSound('mySoundTwo') //preloads sounds/mySoundTwo.ogg
+						//precacheMusic('myMusicTwo') //preloads music/myMusicTwo.ogg
+					
+					// If value 1 is not "blah blah" or "coolswag", it will preload these assets:
+					default:
+						//precacheImage('myImageThree') //preloads images/myImageThree.png
+						//precacheSound('mySoundThree') //preloads sounds/mySoundThree.ogg
+						//precacheMusic('myMusicThree') //preloads music/myMusicThree.ogg
+				}
+		}
+	}
+
+	// Note Hit/Miss
+	override function goodNoteHit(note:Note)
+	{
+		// Code here
+	}
+
+	override function opponentNoteHit(note:Note)
+	{
+		// Code here
+	}
+
+	override function noteMiss(note:Note)
+	{
+		// Code here
+	}
+
+	override function noteMissPress(direction:Int)
+	{
+		// Code here
+	}
+}
